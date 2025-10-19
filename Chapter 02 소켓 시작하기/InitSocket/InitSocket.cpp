026dde758898e277672a288e0aac4d229e93f854
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-10-19 19:30:29 +0900

    STUDY: 소켓 생성과 닫기

--- Chapter 02 소켓 시작하기/InitSocket/InitSocket.cpp
+++ Chapter 02 소켓 시작하기/InitSocket/InitSocket.cpp
@@ -8,6 +8,14 @@ int main(int argc, char* argv[])
 		return 1;
 	printf("[알림] 윈속 초기화 성공\n");
 
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+	printf("[알림] 소켓 생성 성공\n");
+
+	// 소켓 닫기
+	closesocket(sock);
+
 	// 윈속 종료
 	WSACleanup();
 	return 0;
