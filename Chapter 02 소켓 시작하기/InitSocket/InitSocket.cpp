aa722a16c32dd9f7034c7398079a015abb850d4c
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-10-19 19:16:58 +0900

    STUDY: InitSocket.cpp

---
+++ Chapter 02 소켓 시작하기/InitSocket/InitSocket.cpp
@@ -0,0 +1,14 @@
+﻿#include "../../Common.h"
+
+int main(int argc, char* argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+	printf("[알림] 윈속 초기화 성공\n");
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
